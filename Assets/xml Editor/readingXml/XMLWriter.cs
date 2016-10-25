debde959a7a4db1cad214616fcc02cbaa8f048ce
jinsuk3 <j1ntarou.suke@gmail.com>
2016-10-26 02:10:21 +0900

    使ってない変数の削除など

--- Assets/xml Editor/readingXml/XMLWriter.cs
+++ Assets/xml Editor/readingXml/XMLWriter.cs
@@ -18,13 +18,11 @@ using UnityEditor;
         Debug.Log(loadxml);
     }
 }*/
-public class XMLWriter {
+public class XMLWriter{
     public Boolean Write(ref string score)
     {
-        //Boolean flag = false;
-        Boolean dir_flag;
         String scores = "score.xml";
-        String score_locate = "";
+
         try
         {
             
@@ -33,17 +31,18 @@ public class XMLWriter {
             Directory.CreateDirectory("Score");
             String path = Directory.GetCurrentDirectory()+ @"\Score\" + scores; //保存するパスを取得
             Debug.Log(path);
-            Debug.Log("ほげほげ0");
+            
             XmlWriter writer = XmlWriter.Create(path);
-            Debug.Log(writer);
+
             writer.WriteRaw("<MusicData>\n");
             writer.WriteRaw(" <NOTES>\n");
             writer.WriteRaw("  " + score);
             writer.WriteRaw(" </NOTES>\n");
             writer.WriteRaw("<MusicData>\n");
-            Debug.Log("ほげほげ2");
+
             writer.Flush();
             writer.Close();
+            Debug.Log("Save success!");
             return true;
         }
         catch(Exception exc)
@@ -54,77 +53,5 @@ public class XMLWriter {
        // return true;
     }
     
-    /*    string EditFile;
-    public Text XMLData;
-    public Text OpenFile;
-    // Use this for initialization
-    public void OpenXml()
-    {
-        EditFile = OpenFile.text;
-        FileStream fs = null;
-        XmlReader xmlReader = null;
-        XmlReaderSettings settings = null;
-
-        try
-        {
-            XMLData.text += EditFile + "を読み込みます\r\n";
-            fs = new FileStream(EditFile, FileMode.Open);
-
-            settings = new XmlReaderSettings();
-            settings.IgnoreComments = true;
-            settings.IgnoreWhitespace = true;
-
-            xmlReader = XmlReader.Create(fs, settings);
-
-            while (xmlReader.Read() == true)
-            {
-                XmlNodeType nType = xmlReader.NodeType;
-                Debug.Log("NodeType: " + nType.ToString() + "\r\n");
-
-                Debug.Log("LocalName: " + xmlReader.LocalName + "\r\n");
-                Debug.Log("Depth: " + Convert.ToString(xmlReader.Depth) + "\r\n");
-                Debug.Log("Name: " + xmlReader.Name + "\r\n");
-                if (xmlReader.HasValue == true)
-                {
-                    Type valueType = xmlReader.ValueType;
-                    Debug.Log("ValueType: " + valueType.ToString() + "\r\n");
-                    Debug.Log("Value: " + xmlReader.Value + "\r\n");
-                }
-
-                //属性がある場合
-                if (xmlReader.HasAttributes == true)
-                {
-                    for (int i = 0; i < xmlReader.AttributeCount; i++)
-                    {
-                        xmlReader.MoveToAttribute(i);
-                        Debug.Log("Attribute Name: " + xmlReader.Name + "\r\n");
-                        if (xmlReader.HasValue == true)
-                        {
-                            Type valueType = xmlReader.ValueType;
-                            Debug.Log("ValueType: " + valueType.ToString() + "\r\n");
-                            Debug.Log("Attribute Value: " + xmlReader.Value + "\r\n");
-                        }
-                    }
-                    xmlReader.MoveToElement();
-                }
-
-                Debug.Log("\r\n");
-            }
-        }
-        catch (Exception exc)
-        {
-            XMLData.text += "Error: " + exc.Message;
-        }
-        finally
-        {
-            if (fs != null)
-            {
-                fs.Close();
-            }
-            if (xmlReader != null)
-            {
-                xmlReader.Close();
-            }
-        }
-    }*/
+   
 }
