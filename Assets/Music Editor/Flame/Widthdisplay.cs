889b671fee3360d9e13ec0d4ee92f0ca3cd74dde
hotaru789 <takkyu.kudou1211@gmail.com>
2016-10-28 23:44:27 +0900

    10/28

--- Assets/Music Editor/Flame/Widthdisplay.cs
+++ Assets/Music Editor/Flame/Widthdisplay.cs
@@ -8,17 +8,14 @@ public class Widthdisplay : MonoBehaviour {
     private int bunsu;
     private int [] risum = { 4, 8, 12, 16, 24, 32, 48 };
     private GameObject width;
-    private GameObject stoptime;
 
 	void Start () {
         width = GameObject.Find("Widthdisplay");
-        stoptime = GameObject.Find("StopSpeed");
     }
 
     void Update () {
         Width = Notescreate.getmag();
         bunsu = Keyscroll.geti();
         width.GetComponent<Text>().text = "ノーツの幅:Q↓" + Width +"↑W";
-        stoptime.GetComponent<Text>().text = " 停止時間:" + risum[bunsu] + "分 ×" + " E↓" + 0 + "拍 ↑R ";
     }
 }
